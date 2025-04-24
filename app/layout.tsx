a4664b3da9f9831cee427ae8806ed2f6442985e8
murugesh <dhanamurugesh1804@gmail.com>
2025-04-24 12:15:23 +0530

    google ads added

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,25 +23,32 @@ export default function RootLayout({
   const isAdminPage = pathname?.startsWith("/admin")
 
   useEffect(() => {
-    // Load gtag script dynamically (alternative to using <script> tag in <head>)
-    const script1 = document.createElement("script")
-    script1.src = "https://www.googletagmanager.com/gtag/js?id=AW-16982692130"
-    script1.async = true
-    document.head.appendChild(script1)
-
-    const script2 = document.createElement("script")
-    script2.innerHTML = `
-      window.dataLayer = window.dataLayer || [];
-      function gtag(){dataLayer.push(arguments);}
-      gtag('js', new Date());
-      gtag('config', 'AW-16982692130');
+    // Google Tag Manager script injection
+    const script = document.createElement("script")
+    script.innerHTML = `
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-T7445NFW');
     `
-    document.head.appendChild(script2)
+    document.head.appendChild(script)
   }, [])
 
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${inter.variable} ${montserrat.variable} font-sans`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-T7445NFW"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
         <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           <div className="flex min-h-screen flex-col">
             {!isAdminPage && <Navbar />}
