d8b8bcadfba02c08d9234684006166f35ecd885e
murugesh <dhanamurugesh1804@gmail.com>
2025-04-27 00:12:45 +0530

    Done

--- components/footer.tsx
+++ components/footer.tsx
@@ -127,7 +127,10 @@ export function Footer() {
                 <Mail className="h-5 w-5 mr-2 text-gray-400 flex-shrink-0" />
                 <Link href="mailto:info@acquiescent.in" className="text-gray-400 hover:text-white transition-colors">
                 info@acquiescent.in
-                </Link><br/>
+                </Link>
+              </li>
+              <li className="flex">
+                <Mail className="h-5 w-5 mr-2 text-gray-400 flex-shrink-0" />
                 <Link href="mailto:info@acquiescents.in" className="text-gray-400 hover:text-white transition-colors">
                 info@acquiescents.in
                 </Link>
